22793a56176391ce61e6b1d56af64767926b7929
Priyanshu Jayswal <220020047@iitdh.ac.in>
2025-10-21 15:04:46 +0000

    now replace import { companies } from '@/lib/data/question-data'; to tak

--- src/lib/data/question-data.ts
+++ src/lib/data/question-data.ts
@@ -3,18 +3,5 @@
 import { getAllQuestions } from '@/lib/firebase/questions';
 
 
-// Static list of companies for filtering.
-export const companies: string[] = [
-    "Google",
-    "Microsoft",
-    "Amazon",
-    "Meta",
-    "Apple",
-    "Netflix",
-    "Uber",
-    "Airbnb",
-    "Atlassian"
-];
-
 // The function to get questions for the bank is now in `src/lib/firebase/questions.ts`.
 // UI components should call `getAllQuestions()` from there.
